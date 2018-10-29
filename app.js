185a2fdfd861056795056ae8e3652e9eae140458
Jumee-LDN <jmlregister@gmail.com>
2018-10-29 09:39:39 +0000

    Adding changes

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-const express= require('express');
+const express = require('express');
+const app = express();
+const env = require('./config/environment');
+
+const expressLayouts = require('express-ejs-layouts');
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+  res.render('pages/home');
+});
+
+app.use('*', function(req, res, next){
+  console.log('Incoming request', req.method, req.originalUrl);
+  next();
+});
+
+app.listen(env.port, () => {
+  console.log('server is listening...');
+});
